4516167f0d772f56c5aab21d3014a620ddb64878
Cliff Chan <cliffchan1993@gmail.com>
2025-12-23 17:41:09 +0800

    update placeholder

--- components/CategoriesMarquee.jsx
+++ components/CategoriesMarquee.jsx
@@ -6,7 +6,7 @@ const CategoriesMarquee = () => {
             <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-white to-transparent" />
             <div className="flex min-w-[200%] animate-[marqueeScroll_10s_linear_infinite] sm:animate-[marqueeScroll_40s_linear_infinite] group-hover:[animation-play-state:paused] gap-4" >
                 {[...keywords, ...keywords, ...keywords, ...keywords, ...keywords, ...keywords, ...keywords, ...keywords, ...keywords, ...keywords].map((keyword, index) => (
-                    <span key={index} className="px-5 py-2 bg-slate-100 rounded-lg text-slate-500 text-xs sm:text-sm">
+                    <span key={index} className="px-5 py-2 bg-slate-100 rounded-lg text-slate-500 text-xs sm:text-sm whitespace-nowrap">
                         {keyword}
                     </span>
                 ))}
